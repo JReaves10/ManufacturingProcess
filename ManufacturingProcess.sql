76124caf09598f64562cffd81bd74e3378333c44
Jordan Reaves <J_C_Reaves@hotmail.com>
2025-02-10 12:39:12 -0500

    Update ManufacturingProcess.sql

--- ManufacturingProcess.sql
+++ ManufacturingProcess.sql
@@ -1,23 +1,19 @@
 SELECT b.*,
-	   CASE WHEN b.height NOT BETWEEN b.lcl AND b.ucl 
-	        THEN TRUE
-			ELSE FALSE
-	   END AS alert
+       CASE WHEN b.height NOT BETWEEN b.lcl AND b.ucl THEN TRUE
+	    ELSE FALSE END AS alert
 FROM (
 	SELECT a.*,
 	       a.avg_height + 3 * (a.stddev_height/SQRT(5)) AS UCL,
 	       a.avg_height - 3 * (a.stddev_height/SQRT(5)) AS LCL
-    FROM (
+    	FROM (
 		SELECT operator,
-	   		   ROW_NUMBER() OVER moving_totals AS row_number,
-	   		   height,
-	           AVG(height) OVER moving_totals AS avg_height,
-	           STDDEV(height) OVER moving_totals AS stddev_height
-	    FROM manufacturing_parts
-		WINDOW moving_totals AS (
-							PARTITION BY operator
-				            ORDER BY item_no
-	                        ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
+	   	       ROW_NUMBER() OVER moving_totals AS row_number,
+	   	       height,
+	               AVG(height) OVER moving_totals AS avg_height,
+	               STDDEV(height) OVER moving_totals AS stddev_height
+	    	FROM manufacturing_parts
+		WINDOW moving_totals AS (PARTITION BY operatorORDER BY item_no
+			ROWS BETWEEN 4 PRECEDING AND CURRENT ROW
 		)
 	) AS a
 	WHERE a.row_number >= 5
